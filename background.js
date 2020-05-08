8a41116b2aa4de8002cbee72cdcd5f6668296719
Ryan Diaz <ryandiaz1090@gmail.com>
2020-05-08 10:28:01 -0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    document.getElementById("addStreamButton").onclick = function() {
+        let name = document.getElementById("streamId").value;
+
+        chrome.storage.sync.set({'streamer': name}, () => {
+            alert("Success");
+        })
+    }
+};
+
+function addStreamer(status, name, viewers){
+    let tableRef = document.getElementById("onlineStreamersTable");
+    let row = tableRef.insertRow(1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.innerHTML = status;
+    cell2.innerHTML = name;
+    cell3.innerHTML = viewers;
+}
